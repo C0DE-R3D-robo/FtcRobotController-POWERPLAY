be60d567b4dc14828ef4ccbf5fa97f5c9385c550
KyraCrawford <crawfordfamily2006@mail/com>
2022-10-06 11:16:17 -0500

    fucked around with vuforia detection and auton driving
    - KC

--- TeamCode/src/main/java/ObjectDetect.java
+++ TeamCode/src/main/java/ObjectDetect.java
@@ -164,7 +164,7 @@ public class ObjectDetect extends LinearOpMode {
         VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters();
 
         parameters.vuforiaLicenseKey = VUFORIA_KEY;
-        parameters.cameraName = hardwareMap.get(WebcamName.class, "Webcam 1");
+        parameters.cameraName = hardwareMap.get(WebcamName.class, "Webcam 1");;
 
         //  Instantiate the Vuforia engine
         vuforia = ClassFactory.getInstance().createVuforia(parameters);
